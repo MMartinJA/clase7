c6ae79586b75414429a2f45485d6998afd124bc3
MMartinJA <moschinimartin8@gmail.com>
2023-10-09 12:02:41 -0300

    clase 13 js var y if

---
+++ clase13JAVA/ejemplo01.js
@@ -0,0 +1,18 @@
+/*Declaramdo variables*/ 
+const nombre = 'Martin'
+const edad = 23
+
+
+
+
+
+
+
+
+/*como lo vemos */
+
+document.write(nombre + " asi que tienes " + edad);
+
+console.log(nombre)
+console.log(`Mi nombre es ${nombre} y tengo ${edad} años`)
+
