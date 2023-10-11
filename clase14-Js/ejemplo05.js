6313cd113d81510b8ddab6108a14b019b4e8d88d
MMartinJA <moschinimartin8@gmail.com>
2023-10-11 12:06:00 -0300

    clase 14 for, while y funciones

---
+++ clase14-Js/ejemplo05.js
@@ -0,0 +1,23 @@
+const frutas = ['Manzana','Banana','Pera','Palta'];
+
+console.log(frutas)
+
+console.log(frutas.length)
+
+console.log(frutas [1])
+
+var ultimo = frutas[frutas.length-1]
+
+console.log(ultimo)
+
+let seleccionado = 2;
+
+console.log(frutas[seleccionado])
+
+seleccionado = 1;
+
+console.log(frutas[seleccionado])
+
+for(let i = 0; i < frutas.length; i++){
+    console.log('Tenemos ' + frutas[i])
+}
